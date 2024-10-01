0fd4b2bcc8a4dde278c333b65e5cc6b5eed2f6b7
Benjamin Leber <grisu118@grisu118.ch>
2024-10-01 19:07:41 +0200

    ✨ [s5] Go to next s5 automatically

--- enhanced-s5/src/main.ts
+++ enhanced-s5/src/main.ts
@@ -1,18 +1,39 @@
 import { blockHospitals } from "./hospital";
 
 (() => {
-  const transportRequestElem = document.querySelector<HTMLDivElement>(
-    '[data-transport-request="true"]',
-  );
-  if (transportRequestElem) {
-    // we have an open transport request
-    if (
-      transportRequestElem.getAttribute("data-transport-request-type") ===
-      "patient"
-    ) {
-      blockHospitals(transportRequestElem);
-    }
+  if (
+    location.pathname.includes("/patient/") ||
+    location.pathname.includes("/gefangener/")
+  ) {
+    const nextFMSBtn = document.getElementById("next-vehicle-fms-5");
+    const backToMissionBtn = document.getElementById("btn_back_to_mission");
 
-    // TODO handle police transports
+    if (nextFMSBtn) {
+      // go to next fms event
+      nextFMSBtn.click();
+    } else if (backToMissionBtn) {
+      // go back to the mission
+      backToMissionBtn.click();
+    } else {
+      // close the popup
+      window.tellParent("lightboxClose();");
+    }
+  } else {
+    const transportRequestElem = document.querySelector<HTMLDivElement>(
+      '[data-transport-request="true"]',
+    );
+    if (transportRequestElem) {
+      // we have an open transport request
+      if (
+        transportRequestElem.getAttribute("data-transport-request-type") ===
+        "patient"
+      ) {
+        blockHospitals(transportRequestElem);
+      }
+
+      // TODO handle police transports
+
+      return;
+    }
   }
 })();
