3ebd4d9cc4e60450a2cc4dfcc689f4cfb5f69d5d
Benjamin Leber <grisu118@grisu118.ch>
2024-10-04 21:33:42 +0200

    ✨ [aaoMarker] Add key listener to apply marked aao

--- aao-marker/src/main.ts
+++ aao-marker/src/main.ts
@@ -2,6 +2,8 @@ import "./style.css";
 
 const sanitizeString = (txt: string): string => txt.replace(/[#-.]|[[-^]|[?|{}]/g, "\\$&");
 
+const matchClassName = "ls42-lss-aao-match";
+
 (() => {
   const missionGeneralInfoElem = document.getElementById("mission_general_info");
 
@@ -20,17 +22,38 @@ const sanitizeString = (txt: string): string => txt.replace(/[#-.]|[[-^]|[?|{}]/
       const aaoText = elem.textContent?.replace("ß", "ss")?.trim()?.toLowerCase();
       if (aaoText?.match(`^${sanitizeString(missionTitle)}\\*?$`)) {
         // we have a match
-        elem.classList.add("ls42-lss-aao-match");
+        elem.classList.add(matchClassName);
       } else {
         elem.classList.add("ls42-lss-aao-no-match");
       }
     });
   }
 
-  const matchElem = document.querySelector<HTMLAnchorElement>(".tab-pane:has(.aao.ls42-lss-aao-match)");
-  if (matchElem) {
-    const aaoId = matchElem.id;
-    const aaoTabElem = document.querySelector<HTMLAnchorElement>(`#aao-tabs a[href="#${aaoId}"]`);
+  const aaoTabPane = document.querySelector<HTMLAnchorElement>(`.tab-pane:has(.aao.${matchClassName})`);
+  if (aaoTabPane) {
+    const aaoCatId = aaoTabPane.id;
+    const aaoTabElem = document.querySelector<HTMLAnchorElement>(`#aao-tabs a[href="#${aaoCatId}"]`);
     aaoTabElem?.classList?.add("ls42-lss-aao-category-match");
   }
+
+  const resetAAO = () => {
+    document.querySelector<HTMLAnchorElement>(`.aao[reset="true"]`)?.click();
+  };
+
+  const applyMatchedAAO = () => {
+    const aaoElem = document.querySelector<HTMLAnchorElement>(`.aao.${matchClassName}`);
+    aaoElem?.click();
+  };
+
+  // add key event listener
+  document.addEventListener("keypress", (ev) => {
+    switch (ev.key) {
+      case "r":
+        resetAAO();
+        break;
+      case "v":
+        applyMatchedAAO();
+        break;
+    }
+  });
 })();
