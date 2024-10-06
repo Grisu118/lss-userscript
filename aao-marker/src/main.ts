228cd9afe03c6674c43c5d6d9144b37d6a587880
Benjamin Leber <grisu118@grisu118.ch>
2024-10-06 14:43:20 +0200

    ✨ [aaoMarker] Show status of selected aoo

--- aao-marker/src/main.ts
+++ aao-marker/src/main.ts
@@ -1,8 +1,37 @@
 import "./style.css";
+import { checkMedicIncluded } from "./checkMedicIncluded";
 
 const sanitizeString = (txt: string): string => txt.replace(/[#-.]|[[-^]|[?|{}]/g, "\\$&");
+const CALCULATE_TIME = !!document.querySelector(".aao_timer");
 
-const matchClassName = "ls42-lss-aao-match";
+const MATCH_CLASS_NAME = "ls42-lss-aao-match";
+
+const applyAAOStatus = (statusBtn: HTMLAnchorElement) => {
+  aao_available(Number.parseInt(statusBtn.getAttribute("aao_id") ?? "-1"), CALCULATE_TIME);
+
+  const aaoElem = document.querySelector<HTMLAnchorElement>(`.aao.${MATCH_CLASS_NAME}`);
+
+  if (aaoElem) {
+    const aaoAvailable = aaoElem.children.item(0)?.className?.includes("label-success") ?? false;
+    const medicIncluded = checkMedicIncluded(aaoElem);
+
+    statusBtn.classList.remove("btn-success");
+    statusBtn.classList.remove("btn-danger");
+    let text: string;
+    if (aaoAvailable) {
+      statusBtn.classList.add("btn-success");
+      text = "✔️ AAO Available ✔️";
+    } else {
+      statusBtn.classList.add("btn-danger");
+      text = "⚠️⚠️ AAO NOT COMPLETE ⚠️⚠️";
+    }
+    if (medicIncluded) {
+      text = `🚑🚑 ${text} 🚑🚑`;
+    }
+
+    statusBtn.innerText = text;
+  }
+};
 
 (() => {
   const missionGeneralInfoElem = document.getElementById("mission_general_info");
@@ -22,18 +51,36 @@ const matchClassName = "ls42-lss-aao-match";
       const aaoText = elem.textContent?.replace("ß", "ss")?.trim()?.toLowerCase();
       if (aaoText?.match(`^${sanitizeString(missionTitle)}\\*?$`)) {
         // we have a match
-        elem.classList.add(matchClassName);
+        elem.classList.add(MATCH_CLASS_NAME);
       } else {
         elem.classList.add("ls42-lss-aao-no-match");
       }
     });
   }
 
-  const aaoTabPane = document.querySelector<HTMLAnchorElement>(`.tab-pane:has(.aao.${matchClassName})`);
+  const aaoTabPane = document.querySelector<HTMLAnchorElement>(`.tab-pane:has(.aao.${MATCH_CLASS_NAME})`);
   if (aaoTabPane) {
     const aaoCatId = aaoTabPane.id;
     const aaoTabElem = document.querySelector<HTMLAnchorElement>(`#aao-tabs a[href="#${aaoCatId}"]`);
     aaoTabElem?.classList?.add("ls42-lss-aao-category-match");
+  }
+  const aaoElem = document.querySelector<HTMLAnchorElement>(`.aao.${MATCH_CLASS_NAME}`);
+
+  let aaoStatusBtn: HTMLAnchorElement | undefined;
+
+  if (aaoElem) {
+    // we have a match
+    // enforce aao check
+    const aaoId = Number.parseInt(aaoElem.getAttribute("aao_id") ?? "-1");
+    const parentElem = document.getElementById("mission-aao-group");
+    aaoStatusBtn = document.createElement("a");
+    aaoStatusBtn.setAttribute("href", "#");
+    aaoStatusBtn.setAttribute("aao_id", aaoId.toString());
+    aaoStatusBtn.className = "btn btn-xs btn-block";
+
+    applyAAOStatus(aaoStatusBtn);
+
+    parentElem?.prepend(aaoStatusBtn);
   }
 
   const resetAAO = () => {
@@ -41,7 +88,6 @@ const matchClassName = "ls42-lss-aao-match";
   };
 
   const applyMatchedAAO = () => {
-    const aaoElem = document.querySelector<HTMLAnchorElement>(`.aao.${matchClassName}`);
     aaoElem?.click();
   };
 
@@ -53,6 +99,11 @@ const matchClassName = "ls42-lss-aao-match";
         break;
       case "v":
         applyMatchedAAO();
+        break;
+      case "n":
+        if (aaoStatusBtn) {
+          setTimeout(() => applyAAOStatus(aaoStatusBtn), 2000);
+        }
         break;
     }
   });
--- enhanced-s5/typings/global.d.ts
+++
@@ -1,3 +0,0 @@
-interface Window {
-  tellParent(code: string): void;
-}
