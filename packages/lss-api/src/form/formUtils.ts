6fdf277617f0b96cdcb021f401b2d73faad47c04
Benjamin Leber <grisu118@grisu118.ch>
2025-09-26 12:38:08 +0200

    ✨ Add apility to rename a vehicle

--- packages/lss-api/src/form/formUtils.ts
+++ packages/lss-api/src/form/formUtils.ts
@@ -21,3 +21,8 @@ export const modifyForm = async (url: string, modifier: FormModifier) => {
   modifier(formData);
   return fetch(form.action, { method: form.method, body: formData });
 };
+
+export const editVehicle = async (id: number, data: Record<string, string | number>): Promise<Response> =>
+  modifyForm(`/vehicles/${id}/edit`, (formData) => {
+    Object.entries(data).forEach(([key, value]) => formData.set(key, value.toString()));
+  });
