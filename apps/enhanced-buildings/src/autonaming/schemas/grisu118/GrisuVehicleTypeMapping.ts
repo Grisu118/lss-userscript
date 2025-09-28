546080d8422bf144ce94ebfa7410155612b46c53
Benjamin Leber <grisu118@grisu118.ch>
2025-09-28 18:37:34 +0200

    ✨ [ehb] Working renaming

--- apps/enhanced-buildings/src/autonaming/schemas/grisu118/GrisuVehicleTypeMapping.ts
+++ apps/enhanced-buildings/src/autonaming/schemas/grisu118/GrisuVehicleTypeMapping.ts
@@ -5,4 +5,11 @@ export const grisuVehicleTypeMapping: VehicleTypeMapping = {
   [VehicleType.HLF_20]: "HTLF",
   [VehicleType.HLF_10]: "HTLF",
   [VehicleType.HLF_TRAIN]: "HTLF-S",
+  [VehicleType.ELW1]: "KDF",
+  [VehicleType.ELW1_DRONE]: "KDF",
+  [VehicleType.ELW2]: "ELF",
+  [VehicleType.ELW2_DRONE]: "ELF",
+  [VehicleType.MTW]: "PTF",
+  [VehicleType.DLK23]: "HRF",
+  [VehicleType.GWM]: "MGF",
 };
