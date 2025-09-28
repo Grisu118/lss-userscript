356904920cbd38a78dfdd7e4c62777c4e1eb89a5
Benjamin Leber <grisu118@grisu118.ch>
2025-09-28 23:08:21 +0200

    🔨 Complete vehicle type enum

--- apps/enhanced-buildings/src/autonaming/schemas/grisu118/GrisuVehicleTypeMapping.ts
+++ apps/enhanced-buildings/src/autonaming/schemas/grisu118/GrisuVehicleTypeMapping.ts
@@ -2,14 +2,14 @@ import { VehicleType } from "@lss/api";
 import { VehicleTypeMapping } from "../Schema";
 
 export const grisuVehicleTypeMapping: VehicleTypeMapping = {
-  [VehicleType.HLF_20]: "HTLF",
-  [VehicleType.HLF_10]: "HTLF",
-  [VehicleType.HLF_TRAIN]: "HTLF-S",
-  [VehicleType.ELW1]: "KDF",
-  [VehicleType.ELW1_DRONE]: "KDF",
-  [VehicleType.ELW2]: "ELF",
-  [VehicleType.ELW2_DRONE]: "ELF",
-  [VehicleType.MTW]: "PTF",
-  [VehicleType.DLK23]: "HRF",
-  [VehicleType.GWM]: "MGF",
+  [VehicleType.FD_HLF_20]: "HTLF",
+  [VehicleType.FD_HLF_10]: "HTLF",
+  [VehicleType.FD_HLF_TRAIN]: "HTLF-S",
+  [VehicleType.FD_ELW1]: "KDF",
+  [VehicleType.FD_ELW1_DRONE]: "KDF",
+  [VehicleType.FD_ELW2]: "ELF",
+  [VehicleType.FD_ELW2_DRONE]: "ELF",
+  [VehicleType.FD_MTW]: "PTF",
+  [VehicleType.FD_DLK23]: "HRF",
+  [VehicleType.FD_GWM]: "MGF",
 };
