fbe60da7e1089e27f3c3b5eea7c578d23572e2a5
Benjamin Leber <grisu118@grisu118.ch>
2025-09-28 17:45:32 +0200

    ✨ [ehb] Show naming mismatches if there is a schema

---
+++ apps/enhanced-buildings/src/autonaming/autonaming.ts
@@ -0,0 +1,20 @@
+import { Building } from "@lss/api";
+import { registerSchemas } from "./schemas/registerSchemas";
+import { SchemaRegistry } from "./schemas/SchemaRegistry";
+import { VehicleManager } from "./VehicleManager";
+
+registerSchemas();
+
+export const autonaming = (currentBuilding: Building): void => {
+  console.log("Load Autonaming for building", currentBuilding);
+  // lookup if we have a naming scheme for this building
+  const schema = SchemaRegistry.INSTANCE.getForBuilding(currentBuilding);
+  if (!schema) {
+    return;
+  }
+  console.log("Found schema for building", schema);
+
+  // setup vehicle manager
+  const vehicleManager = new VehicleManager(schema, currentBuilding);
+  vehicleManager.displayNamingStatus();
+};
