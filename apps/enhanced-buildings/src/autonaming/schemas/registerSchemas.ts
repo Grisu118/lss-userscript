2662025d50b5b05d5db92f292c6b47ce0efb3055
Benjamin Leber <grisu118@grisu118.ch>
2025-10-15 21:13:33 +0200

    ✨ [ehb] Adapt and extend mapping for police

--- apps/enhanced-buildings/src/autonaming/schemas/registerSchemas.ts
+++ apps/enhanced-buildings/src/autonaming/schemas/registerSchemas.ts
@@ -1,3 +1,11 @@
+import {
+  agFederalPoliceStation,
+  agFederalRegionalPoliceStation,
+  agFederalRegionalPoliceStation2,
+  agPoliceStation,
+  agRegionalPoliceStation,
+  agRegionalPoliceStation2,
+} from "./grisu118/canton/police";
 import { grisuVehicleTypeMapping } from "./grisu118/GrisuVehicleTypeMapping";
 import { relaFireDepartment } from "./grisu118/rela/fire";
 import { relaMedic, relaSeg } from "./grisu118/rela/medic";
@@ -7,5 +15,17 @@ import { SchemaRegistry } from "./SchemaRegistry";
 export const registerSchemas = () => {
   SchemaRegistry.INSTANCE.registerGlobalVehicleTypeMapping(grisuVehicleTypeMapping);
 
-  SchemaRegistry.INSTANCE.register(relaFireDepartment, relaMedic, relaSeg, relaPoliceStation, relaFederalPoliceStation);
+  SchemaRegistry.INSTANCE.register(
+    relaFireDepartment,
+    relaMedic,
+    relaSeg,
+    relaPoliceStation,
+    relaFederalPoliceStation,
+    agPoliceStation,
+    agFederalPoliceStation,
+    agRegionalPoliceStation,
+    agFederalRegionalPoliceStation,
+    agRegionalPoliceStation2,
+    agFederalRegionalPoliceStation2,
+  );
 };
